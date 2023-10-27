5778274f8f7a4b79e7155479b99aab93950e9235
José Daniel Hernández Osorio <jdanieldev@gmail.com>
2023-10-27 12:54:00 -0500

    First commit

---
+++ product-service/src/main/java/academy/digitallab/store/service/product/entity/Category.java
@@ -0,0 +1,17 @@
+package academy.digitallab.store.service.product.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tbl_categories")
+@Data @AllArgsConstructor @NoArgsConstructor @Builder
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+}
